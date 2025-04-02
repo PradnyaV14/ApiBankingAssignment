84a53da76ea3cea822b1e98f6a055366ce93bc27
PradnyaV14 <38156170+PradnyaV14@users.noreply.github.com>
2025-04-02 23:35:30 +0530

    Add files via upload

---
+++ import.sql
@@ -0,0 +1,8 @@
+/* CAN BE USED WHEN TESTING VIA POSTMAN
+INSERT INTO Product (id, name, description, price, quantity) VALUES 
+(1, 'Laptop', 'High-end gaming laptop', 1499.99, 5),
+(2, 'Mouse', 'Wireless gaming mouse', 49.99, 10),
+(3, 'Keyboard', 'Mechanical gaming keyboard', 89.99, 7),
+(4, 'Monitor', '27-inch 4K UHD Monitor', 399.99, 3),
+(5, 'Headset', 'Noise-canceling gaming headset', 129.99, 8);
+*/
